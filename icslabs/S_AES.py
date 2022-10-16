5e782c870a90159084a984946a23e98a11b34871
Preetam Maske <55644679+Preetam3620@users.noreply.github.com>
2022-10-16 20:00:43 +0530

    Test case changed

--- icslabs/S_AES.py
+++ icslabs/S_AES.py
@@ -1,6 +1,6 @@
 # S boxe
-sbox = [9, 4, 10, 11, 13, 1, 8, 5, 6, 2, 0, 3, 12, 14, 15, 7]
-
+# sbox = [9, 4, 10, 11, 13, 1, 8, 5, 6, 2, 0, 3, 12, 14, 15, 7]
+zbox = [ 8, 5, 6, 2, 0, 9, 4, 10, 11, 13, 1, 3, 12, 14, 15, 7]
 
 def convertNumToAsciiBit(x): # coverts decimal to binary
     y = ""
